7032bfe1001238a19e11851897b5fdae481bc333
Nocteln <eliottmieze@gmail.com>
2023-08-05 00:22:19 +0200

    ajout du flappy

--- flappy/script.js
+++ flappy/script.js
@@ -1,53 +1,93 @@
-var block = document.getElementById("block");
-var hole = document.getElementById("hole");
-var character = document.getElementById("character");
-var jumping = 0;
-var counter = 0;
+const canvas = document.getElementById("canvas")
+const ctx=canvas.getContext("2d")
+const img = new Image()
+img.src = "../assets/img/flappy-bird-set.png"
 
-hole.addEventListener("animationiteration", () => {
-  var random = -(Math.random() * 300 + 150);
-  hole.style.top = random + "px";
-  counter++;
-});
-setInterval(function () {
-  var characterTop = parseInt(
-    window.getComputedStyle(character).getPropertyValue("top")
-  );
-  if (jumping == 0) {
-    character.style.top = characterTop + 3 + "px";
-  }
-  var blockLeft = parseInt(
-    window.getComputedStyle(block).getPropertyValue("left")
-  );
-  var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-  var cTop = -(500 - characterTop);
-  if (
-    characterTop > 480 ||
-    (blockLeft < 20 &&
-      blockLeft > -50 &&
-      (cTop < holeTop || cTop > holeTop + 130))
-  ) {
-    alert("Game over. Score: " + (counter - 1));
-    character.style.top = 100 + "px";
-    counter = 0;
-  }
-}, 10);
+// Generals settings
+let gamePlaying = false;
+const gravity = .5;
+const speed = 6.2;
+const size = [51,36]
+const jump = -11.5
+const cTenth = (canvas.width /10)
 
-function jump() {
-  jumping = 1;
-  let jumpCount = 0;
-  var jumpInterval = setInterval(function () {
-    var characterTop = parseInt(
-      window.getComputedStyle(character).getPropertyValue("top")
-    );
-    if (characterTop > 6 && jumpCount < 15) {
-      character.style.top = characterTop - 5 + "px";
+// Pipes Settings
+const pipeWidth = 78    
+const pipeGap  = 270
+const pipeLoc = ( )=> (Math.random() * ((canvas.height - (pipeGap +pipeWidth)) - pipeWidth)) +pipeWidth
+
+let index = 0, bestScore = 0, currentScore = 0, pipes = [], flight, flightHeight;
+
+const setup = () => {
+    currentScore = 0;
+    flight = jump
+    flightHeight = (canvas.height/2) - (size[1] /2)
+
+    pipes = Array(3).fill().map((a, i) => [canvas.height + (i*(pipeGap + pipeWidth)), pipeLoc()])
+}
+
+const render = () => {
+    index++
+
+    // Background
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index*(speed/2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height)
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index*(speed/2)) % canvas.width), 0, canvas.width, canvas.height)
+
+
+    if(gamePlaying) {
+        ctx.drawImage(img, 432, Math.floor((index % 9) /3 ) * size[1], ...size, cTenth, flightHeight, ...size)
+        flight += gravity
+        flightHeight = Math.min(flightHeight + flight, canvas.height -size[1])
+
+    } else {
+        ctx.drawImage(img, 432, Math.floor((index % 9) /3 ) * size[1], ...size, ((canvas.width / 2 )- size[0] /2 ), flightHeight, ...size)
+    flightHeight = (canvas.height /2) - (size[1] /2)
+
+    ctx.fillText(`Meilleur score : ${bestScore}`, 55, 245)
+    ctx.fillText("Cliquez pour Jouer !", 45, 535)
+    ctx.font = "bold 30px courier"
     }
-    if (jumpCount > 20) {
-      clearInterval(jumpInterval);
-      jumping = 0;
-      jumpCount = 0;
+
+    // Pipe Display
+    if(gamePlaying) {
+        pipes.map(pipe => {
+            pipe[0] -= speed
+
+            //Top pipe
+            ctx.drawImage(img, 432,588-pipe[1], pipeWidth,pipe[1],pipe[0], 0, pipeWidth, pipe[1])
+
+            //bottom pipe
+                ctx.drawImage(img, 432+pipeWidth, 108, pipeWidth, canvas.height-pipe[1] + pipeGap, pipe[0], pipe[1]+pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap)
+            if(pipe[0] <= -pipeWidth) {
+                currentScore++
+                bestScore =Math.max(bestScore, currentScore)
+                
+                // remove pipe & crete new
+                pipes = [...pipes.slice(1), [pipes[pipes.length-1][0] + pipeGap  +pipeWidth, pipeLoc()]]
+            }
+
+            if ([
+                pipe[0]<= cTenth+size[0],
+                pipe[0]+pipeWidth >= cTenth,
+                pipe[1] > flightHeight || pipe[1] + pipeGap < flightHeight + size[1]
+            ].every(elem => elem)) {
+                gamePlaying = false
+                setup()
+            }
+        })
     }
-    jumpCount++;
-  }, 10);
+    
+
+    document.getElementById("record").innerHTML = bestScore
+    document.getElementById("score").innerHTML = currentScore
+
+    window.requestAnimationFrame(render)
+}
+
+setup()
+img.onload = render
+
+document.addEventListener("click", ( )=> gamePlaying = true)
+window.onclick = () => {
+    flight = jump
 }
